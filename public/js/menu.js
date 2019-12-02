5bc75cdc2ab8989049758b329791fb0ffd9b4faf
kingen-i-bingen <57368863+kingen-i-bingen@users.noreply.github.com>
2019-12-02 12:27:03 +0100

    Menu

---
+++ public/js/menu.js
@@ -0,0 +1,37 @@
+var food = [
+  {
+    "name":"Kycklingburgare",
+    "kCal": 500,
+    "lactose": false,
+    "gluten": true,
+    "pic": "img/burgare2.jpg"
+  },
+  {
+    "name":"Klassiker",
+    "kCal": 750,
+    "lactose": true,
+    "gluten": true,
+    "pic": "img/burgare1.jpg"
+  },
+  {
+    "name":"Veganburgare",
+    "kCal": 600,
+    "lactose": false,
+    "gluten": false,
+    "pic": "img/burgare3.jpg"
+  },
+  {
+    "name":"Halloumiburgare",
+    "kCal": 650,
+    "lactose": true,
+    "gluten": true,
+    "pic": "img/burgare4.jpg"
+  },
+  {
+    "name":"Baconburgare",
+    "kCal": 800,
+    "lactose": true,
+    "gluten": true,
+    "pic": "img/burgar5.jpg"
+  }
+]
